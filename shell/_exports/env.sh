37f67b6fb8f125e3c4ca36e2fefdf190932796a4
Rubén Zagon <ruben.zagon@gmail.com>
2020-12-20 10:48:50 +0000

    Auto source

--- shell/_exports/env.sh
+++ shell/_exports/env.sh
@@ -1,1 +1,3 @@
+export DOTFILES_PATH=/Users/rubenzagon/.dotfiles/
 export NAVI_PATH=/Users/rubenzagon/.dotfiles/doc/navi/
+export HOMEBREW_BUNDLE_FILE_PATH=/Users/rubenzagon/.dotfiles/so/mac/brew/
---
+++ shell/_functions/export_apps.sh
@@ -0,0 +1,8 @@
+function export_app() {
+    brew services stop --all
+    brew bundle dump --file="$HOMEBREW_BUNDLE_FILE_PATH" --force
+    echo 'Brew apps exported!'
+
+    ls -1 /usr/local/lib/node_modules | grep -v npm >"$DOTFILES_PATH/langs/js/global_modules.txt"
+    echo 'NPM apps exported!'
+}
