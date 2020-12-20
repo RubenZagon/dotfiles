3153e2e1c2f28899fc3883baa8c9dcc9d9071903
Rubén Zagon <ruben.zagon@gmail.com>
2020-12-20 09:36:56 +0000

    🚚 Move files to folders

---
+++ symlinks/links.sh
@@ -0,0 +1,5 @@
+ln -s "$HOME/.dotfiles/git/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/.dotfiles/git/.gitignore_global" "$HOME/.gitignore_global"
+ln -s "$HOME/.dotfiles/shell/zsh/.zshrc" "$HOME/.zshrc"
+ln -s "$HOME/.dotfiles/shell/zsh/.zlogin" "$HOME/.zlogin"
+ln -s "$HOME/.dotfiles/shell/zsh/.zshenv" "$HOME/.zshenv"
